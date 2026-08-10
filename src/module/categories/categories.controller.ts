57a85a307cf49398e3b5d14a3e0f46ee29bdd565
unknown <abuhasanxt@gmail.com>
2026-08-10 23:28:12 +0600

    creating send response file

--- src/module/categories/categories.controller.ts
+++ src/module/categories/categories.controller.ts
@@ -1,44 +1,51 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
 import { categoriesService } from "./categories.service";
 import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sentResponse";
+
+
 
 const createCategories = catchAsync(async (req: Request, res: Response) => {
   const result = await categoriesService.createCategories(req.body);
-  res.status(201).json({
-    success: false,
-    message: "Category create successfully",
+  sendResponse(res, {
+    httpStatusCode: 200,
+    success: true,
+    message: "Category Create successfully",
     data: result,
   });
 });
 
+
 const getAllCategory = catchAsync(async (req: Request, res: Response) => {
   const result = await categoriesService.getAllCategory();
-  res.status(200).json({
-    success: true,
-    message: "Category fetch successfully",
-    data: result,
-  });
+  sendResponse(res,{
+   httpStatusCode:201,
+   success:true,
+   message:"Categories fetching successfully",
+   data:result
+  })
 });
 
 const updateCategory = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await categoriesService.updateCategory(id as string, req.body);
-  res.status(200).json({
-    success: true,
-    message: "Category update successfully",
-    data: result,
-  });
+  sendResponse(res,{
+   httpStatusCode:200,
+   success:true,
+   message:"Category update successfully",
+   data:result
+  })
 });
 
 const deleteCategory = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await categoriesService.deleteCategory(id as string);
-  res.status(200).json({
-    success: true,
-    message: "Delete successfully",
-    data: result,
-  });
+   sendResponse(res,{
+   httpStatusCode:200,
+   success:true,
+   message:"Category Delete successfully",
+   data:result
+  })
 });
 export const categoriesController = {
   createCategories,
