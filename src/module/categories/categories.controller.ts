0fe12759d4b560f81ee3ac5881ef61fa0255919d
unknown <abuhasanxt@gmail.com>
2026-08-12 22:30:05 +0600

    add to http-status code

--- src/module/categories/categories.controller.ts
+++ src/module/categories/categories.controller.ts
@@ -2,13 +2,14 @@ import { Request, Response } from "express";
 import { categoriesService } from "./categories.service";
 import { catchAsync } from "../../shared/catchAsync";
 import { sendResponse } from "../../shared/sentResponse";
+import status from "http-status";
 
 
 
 const createCategories = catchAsync(async (req: Request, res: Response) => {
   const result = await categoriesService.createCategories(req.body);
   sendResponse(res, {
-    httpStatusCode: 200,
+    httpStatusCode: status.CREATED,
     success: true,
     message: "Category Create successfully",
     data: result,
@@ -19,7 +20,7 @@ const createCategories = catchAsync(async (req: Request, res: Response) => {
 const getAllCategory = catchAsync(async (req: Request, res: Response) => {
   const result = await categoriesService.getAllCategory();
   sendResponse(res,{
-   httpStatusCode:201,
+   httpStatusCode:status.OK,
    success:true,
    message:"Categories fetching successfully",
    data:result
@@ -30,7 +31,7 @@ const updateCategory = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await categoriesService.updateCategory(id as string, req.body);
   sendResponse(res,{
-   httpStatusCode:200,
+   httpStatusCode:status.CREATED,
    success:true,
    message:"Category update successfully",
    data:result
@@ -41,7 +42,7 @@ const deleteCategory = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await categoriesService.deleteCategory(id as string);
    sendResponse(res,{
-   httpStatusCode:200,
+   httpStatusCode:status.OK,
    success:true,
    message:"Category Delete successfully",
    data:result
